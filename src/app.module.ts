8851eeac71c8f24883e4d00055c70d97891dd511
Aliyu Efe <aliyuefe9501@gmail.com>
2025-08-10 16:11:29 +0100

    move customer to module folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { CustomersModule } from './modules/customers/customers.module';
+import { RestaurantsModule } from './modules/restaurants/restaurants.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, CustomersModule, RestaurantsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }

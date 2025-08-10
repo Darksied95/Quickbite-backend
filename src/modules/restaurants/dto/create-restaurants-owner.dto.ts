17d8f79b1377e710be9a3be7bf55d43bade7f5f7
Aliyu Efe <aliyuefe9501@gmail.com>
2025-08-10 16:12:24 +0100

    add restuarant owner register dto

---
+++ src/modules/restaurants/dto/create-restaurants-owner.dto.ts
@@ -0,0 +1,49 @@
+//email ,password , phone, restaurantName, description , cusineTypes, restaurantAddress , businessLicense, 
+
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import { Transform, Type } from "class-transformer";
+import { IsArray, IsEmail, IsNotEmpty, IsOptional, IsPhoneNumber, IsString, MaxLength, MinLength, ValidateNested } from "class-validator";
+import { AddressDto } from "src/common/dto/address.dto";
+
+
+export class CreateRestaurantsOwnerDto {
+    @ApiProperty({ example: "star@gmail.com" })
+    @IsEmail()
+    @Transform(({ value }) => value.toLowerCase().trim())
+    email: string
+
+    @ApiProperty()
+    @IsString()
+    @MinLength(8)
+    password: string
+
+    @ApiProperty({ example: "+2347012345678" })
+    @IsPhoneNumber("NG")
+    phone: string
+
+    @ApiProperty({ example: "Rajah Cuisine" })
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(255)
+    restuarantName: string
+
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    description?: string
+
+    @ApiProperty({ example: ["Nigerian", "Continental"] })
+    @IsArray()
+    @IsString({ each: true })
+    cuisineTypes: Array<string>
+
+    @ApiProperty({ example: "BXH1239PIO" })
+    @IsOptional()
+    @IsString()
+    businessLicense?: string
+
+    @ApiProperty()
+    @ValidateNested()
+    @Type(() => AddressDto)
+    address: AddressDto
+}

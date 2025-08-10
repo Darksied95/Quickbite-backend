b5ce5b7a9a2a15deae71e0f3ea3c470a94d8cf0c
Aliyu Efe <aliyuefe9501@gmail.com>
2025-08-10 15:56:19 +0100

    create address dto

---
+++ src/common/dto/address.dto.ts
@@ -0,0 +1,41 @@
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class AddressDto {
+    @ApiProperty({ example: "Home" })
+    @IsString()
+    @IsNotEmpty()
+    label: string
+
+    @ApiProperty({ example: "123 street road" })
+    @IsString()
+    @IsNotEmpty()
+    addressLine1: string
+
+    @ApiPropertyOptional({ example: "Near Uyo" })
+    @IsString()
+    @IsOptional()
+    addressLine2?: string
+
+    @ApiProperty({ example: "Uyo" })
+    @IsString()
+    @IsNotEmpty()
+    city: string
+
+
+    @ApiProperty({ example: "Akwa Ibom State" })
+    @IsString()
+    @IsNotEmpty()
+    state: string
+
+    @ApiProperty({ example: "Nigeria" })
+    @IsString()
+    @IsNotEmpty()
+    country: string
+
+    @ApiProperty({ example: "M20 4UR" })
+    @IsString()
+    @IsNotEmpty()
+    postalCode: string
+
+}

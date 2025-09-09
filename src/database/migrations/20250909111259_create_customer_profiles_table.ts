1efa6174559d94cd773de77e1d342048afa46eeb
Aliyu Efe <aliyuefe9501@gmail.com>
2025-09-09 12:52:37 +0100

    create customers profile table migrartions

---
+++ src/database/migrations/20250909111259_create_customer_profiles_table.ts
@@ -0,0 +1,18 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable("customer_profiles", function (table) {
+        table.uuid("id").primary().defaultTo(knex.raw('gen_random_uuid()'))
+        table.uuid("user_id").notNullable().references("id").inTable("users").onDelete("CASCADE").onUpdate("CASCADE")
+        table.uuid("address_id").references("id").inTable("addresses").onDelete("CASCADE").onUpdate("CASCADE")
+        table.integer("total_orders").notNullable().defaultTo(0)
+        table.decimal("total_spent")
+        table.timestamps(true, true)
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+}
+

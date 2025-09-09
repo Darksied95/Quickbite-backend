d319322f4a56c52807f582d5a67e7752398e89f1
Aliyu Efe <aliyuefe9501@gmail.com>
2025-09-09 12:52:14 +0100

    create users table migrations

---
+++ src/database/migrations/20250823125937_create_users_table.ts
@@ -0,0 +1,21 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('users', function (table) {
+        table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+        table.string('email', 255).unique().notNullable();
+        table.string('first_name', 255).notNullable();
+        table.string('last_name', 255).notNullable();
+        table.string('password', 255).notNullable();
+        table.string('phone', 20).notNullable();
+        table.enum('user_type', ['driver', 'customer', 'restaurant']).notNullable();
+        table.timestamps(true, true)
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable('users')
+}
+

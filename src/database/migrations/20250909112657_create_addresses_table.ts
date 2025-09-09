669db3ada2875f9d0cd81bb3d9ba061da1656c22
Aliyu Efe <aliyuefe9501@gmail.com>
2025-09-09 12:45:55 +0100

    add create_addresses migration

---
+++ src/database/migrations/20250909112657_create_addresses_table.ts
@@ -0,0 +1,29 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable("addresses", function (table) {
+        table.uuid("id").primary().defaultTo(knex.raw("gen_random_uuid()"));
+        table.uuid("user_id").notNullable().references("id").inTable("users").onDelete("CASCADE").onUpdate("CASCADE");
+        table.string("label").notNullable()
+        table.string("street_address").notNullable()
+        table.string("apartment_unit")
+        table.string("city").notNullable()
+        table.string("state").notNullable()
+        table.string("postal_code").notNullable()
+        table.string("country").notNullable()
+        table.boolean("is_default").notNullable().defaultTo(false)
+        table.timestamps(true, true)
+    }).then(() => {
+        return knex.raw(`
+            CREATE UNIQUE INDEX addressess_user_default_unique
+            ON addressess(user_id)
+            WHERE is_default= true
+            `)
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+}
+

9b6e12b6e81904af3561f6cf1b62b0c20551fec6
Aliyu Efe <aliyuefe9501@gmail.com>
2025-09-12 03:36:24 +0100

    create address repository

---
+++ src/modules/addresses/addresses.repository.ts
@@ -0,0 +1,16 @@
+import { Knex } from "knex";
+import { CreateAddressDto } from "./dto/create-address.dto";
+import { IAddress } from "./addresses.types";
+
+type Address = (CreateAddressDto & { user_id: string })[]
+interface IAddresses {
+    create(addresses: Address, Trx: Knex.Transaction): Promise<IAddress[] | null>
+}
+
+export class AddressesRepository implements IAddresses {
+
+    async create(addresses: Address, trx: Knex.Transaction) {
+        const results = trx("addresses").insert(addresses).returning("*")
+        return results || null
+    }
+}

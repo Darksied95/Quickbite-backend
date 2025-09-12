c53b2f0032f007592bd4034b55a4573769c59b85
Aliyu Efe <aliyuefe9501@gmail.com>
2025-09-12 03:38:00 +0100

    create customer dto

--- src/modules/customers/dto/create-customers.dto.ts
+++ src/modules/customers/dto/create-customers.dto.ts
@@ -1,42 +1,5 @@
-//email ,password, phone , firstName , lastName, DOB, address
+import { CreateUserDTO } from "src/modules/users/dto/create-user.dto";
 
-import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
-import { Transform, Type } from "class-transformer";
-import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString, MinLength, IsDateString, IsOptional, ValidateNested } from "class-validator";
-import { AddressDto } from "src/common/dto/address.dto";
+export class CreateCustomerDto extends CreateUserDTO {
 
-export class CreateCustomerDto {
-    @ApiProperty({ example: "email@quickbite.com" })
-    @IsEmail()
-    @Transform(({ value }) => value.toLowerCase().trim())
-    email: string
-
-    @ApiProperty()
-    @IsString()
-    @MinLength(8)
-    password: string
-
-    @ApiProperty({ example: "+2347012345678" })
-    @IsPhoneNumber("NG")
-    phone: string
-
-    @ApiProperty({ example: "Rajah" })
-    @IsString()
-    @MinLength(3)
-    firstName: string
-
-    @ApiProperty({ example: "Jojo" })
-    @IsString()
-    @MinLength(3)
-    lastName: string
-
-    @ApiPropertyOptional({ example: "1990-05-30" })
-    @IsOptional()
-    @IsDateString()
-    dateOfBirth?: string
-
-    @ApiProperty()
-    @ValidateNested()
-    @Type(() => AddressDto)
-    address: AddressDto
 }

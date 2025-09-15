858ed9d6345bffacc794ab1191e7453383e04fd7
Aliyu Efe <aliyuefe9501@gmail.com>
2025-09-15 21:11:16 +0100

    create exception filter

---
+++ src/exceptions/domain-exception.filter.ts
@@ -0,0 +1,30 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus } from "@nestjs/common";
+import { UserNotFoundException } from "./user-not-found.exception";
+import { Request, Response } from "express";
+
+
+@Catch(UserNotFoundException)
+export class DomainExceptionFilter implements ExceptionFilter {
+    catch(exception: UserNotFoundException, host: ArgumentsHost) {
+        const ctx = host.switchToHttp()
+        const response = ctx.getResponse<Response>()
+        const request = ctx.getRequest<Request>()
+
+        let statusCode = HttpStatus.NOT_FOUND
+        let message = exception.message
+
+        console.log(request.url);
+
+        if (request.url.includes("/auth/login")) {
+            statusCode = HttpStatus.UNAUTHORIZED
+            message = 'Invalid credentials.'
+        }
+
+        response.status(statusCode).json({
+            message,
+            timestamp: new Date().toISOString(),
+            path: request.url,
+            statusCode
+        })
+    }
+}

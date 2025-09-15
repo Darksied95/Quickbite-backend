45a732977bcbcec1f9d005963c2fee2124ced6ca
Aliyu Efe <aliyuefe9501@gmail.com>
2025-09-15 21:10:46 +0100

    add get user by email to userService

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,33 +1,44 @@
-import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
-import { Knex } from "knex";
-import { InjectConnection } from "nest-knexjs";
-import { UserRepository } from "./users.repository";
-import { CreateUserDTO } from "./dto/create-user.dto";
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
+import { Knex } from 'knex';
+import { InjectConnection } from 'nest-knexjs';
+import { UserRepository } from './users.repository';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UserNotFoundException } from 'src/exceptions/user-not-found.exception';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectConnection() private readonly knex: Knex,
-        private readonly userRepository: UserRepository
-    ) { }
 
-    async create(userData: Omit<CreateUserDTO, "addresses">, trx: Knex.Transaction) {
+  constructor(
+    @InjectConnection() private readonly knex: Knex,
+    private readonly userRepository: UserRepository,
+  ) { }
 
-        const existingUser = await this.knex("users").where({ email: userData.email }).first()
+  async create(
+    userData: Omit<CreateUserDTO, 'addresses'>,
+    trx: Knex.Transaction,
+  ) {
+    const existingUser = await this.userRepository.findByEmail(userData.email)
 
-        console.log(existingUser);
+    if (existingUser) throw new ConflictException('User with this email already exists');
 
-        if (existingUser) {
-            throw new ConflictException('User with this email already exists')
-        }
+    const user = await this.userRepository.create(userData, trx);
 
-        const user = await this.userRepository.create(userData, trx)
+    if (!user) throw new InternalServerErrorException('Failed to create user');
 
-        if (!user) {
-            throw new InternalServerErrorException("Failed to create user")
-        }
+    return user;
+  }
 
-        return user
-    }
+  async getUserWithEmail(email: string) {
 
+    const user = await this.userRepository.findByEmail(email)
+
+    if (!user) throw new UserNotFoundException(email)
+
+    return user
+  }
 }

bed749b9887917f67de283fb88078117cc541a94
Aliyu Efe <aliyuefe9501@gmail.com>
2025-09-15 21:11:02 +0100

    add tokens type to db type list

--- src/database/database.types.ts
+++ src/database/database.types.ts
@@ -1,12 +1,14 @@
-import { IUser } from "src/modules/users/users.type";
-import { Knex } from "knex";
-import { ICustomerProfile } from "src/modules/customers/customer.types";
-import { IAddress } from "src/modules/addresses/addresses.types";
+import { IUser } from 'src/modules/users/users.type';
+import { Knex } from 'knex';
+import { ICustomerProfile } from 'src/modules/customers/customer.types';
+import { IAddress } from 'src/modules/addresses/addresses.types';
+import { IToken } from 'src/modules/auth/token/token.type';
 
-export type TypedKnex = Knex<DatabaseSchema>
+export type TypedKnex = Knex<DatabaseSchema>;
 
 export interface DatabaseSchema {
-    "users": IUser,
-    "customer_profiles": ICustomerProfile,
-    "addresses": IAddress
+  users: IUser;
+  customer_profiles: ICustomerProfile;
+  addresses: IAddress;
+  tokens: IToken
 }

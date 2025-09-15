20d6f42a5a04a5ba501b2e38d9d75c956ff4bcc3
Aliyu Efe <aliyuefe9501@gmail.com>
2025-09-15 21:13:14 +0100

    add login functionality with jwt

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,40 +1,46 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { CreateCustomerDto } from "../customers/dto/create-customers.dto";
-import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
-import { CustomerResponseDto } from "../customers/dto/customer-response.dto";
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateCustomerDto } from '../customers/dto/create-customers.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CustomerResponseDto } from '../customers/dto/customer-response.dto';
+import { LoginRequestDTO, LoginResponseDTO } from './dto/login.dto';
 
-@ApiTags("auth")
+
+
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) { }
 
-    constructor(private readonly authService: AuthService) { }
+  @Post('login')
+  async login(@Body() loginDetails: LoginRequestDTO): Promise<LoginResponseDTO> {
+    const result = await this.authService.login(loginDetails)
+    return result
+  }
 
-    @Post('login')
-    login(): string {
-        return "login"
-    }
+  @Post('register/customer')
+  @ApiOperation({ summary: 'Register a new customer' })
+  @ApiResponse({ status: 201, description: 'Customer successfully registered' })
+  async registerCustomer(
+    @Body() req: CreateCustomerDto,
+  ): Promise<CustomerResponseDto> {
+    return await this.authService.createCustomer(req);
+  }
 
-    @Post('register/customer')
-    @ApiOperation({ summary: "Register a new customer" })
-    @ApiResponse({ status: 201, description: "Customer successfully registered" })
-    async registerCustomer(@Body() req: CreateCustomerDto): Promise<CustomerResponseDto> {
-        return await this.authService.createCustomer(req)
-    }
+  @Post('register/driver')
+  @ApiOperation({ summary: 'Register a new driver' })
+  @ApiResponse({ status: 201, description: 'Driver successfully registered' })
+  async registerDriver(@Body() req: CreateCustomerDto) {
+    return;
+  }
 
-    @Post('register/driver')
-    @ApiOperation({ summary: "Register a new driver" })
-    @ApiResponse({ status: 201, description: "Driver successfully registered" })
-    async registerDriver(@Body() req: CreateCustomerDto) {
-        return
-    }
-
-    @Post('register/restaurant')
-    @ApiOperation({ summary: "Register a new restaurant owner" })
-    @ApiResponse({ status: 201, description: "Restaurant successfully registered" })
-    registerRestauranOwner(@Body() req: CreateCustomerDto) {
-        return
-    }
-
-
+  @Post('register/restaurant')
+  @ApiOperation({ summary: 'Register a new restaurant owner' })
+  @ApiResponse({
+    status: 201,
+    description: 'Restaurant successfully registered',
+  })
+  registerRestauranOwner(@Body() req: CreateCustomerDto) {
+    return;
+  }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,49 +1,95 @@
-import { Knex } from "knex";
-import { Injectable } from "@nestjs/common"
-import { InjectConnection } from "nest-knexjs";
-import { UserService } from "../users/users.service";
-import { PasswordService } from "./password.service";
-import { AddressService } from "../addresses/addresses.service";
-import { CustomersService } from "../customers/customers.service";
-import { CreateCustomerDto } from "../customers/dto/create-customers.dto";
+import { Knex } from 'knex';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectConnection } from 'nest-knexjs';
+import { UserService } from '../users/users.service';
+import { HashingService } from './hashing.service';
+import { AddressService } from '../addresses/addresses.service';
+import { CustomersService } from '../customers/customers.service';
+import { CreateCustomerDto } from '../customers/dto/create-customers.dto';
+import { LoginRequestDTO } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { UserNotFoundException } from 'src/exceptions/user-not-found.exception';
+import { TokenService } from './token/token.service';
+import { plainToInstance } from 'class-transformer';
+import { UserResponseDto } from '../users/dto/user-response.dto';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        @InjectConnection() private readonly knex: Knex,
-        private readonly customerService: CustomersService,
-        private readonly userService: UserService,
-        private readonly addressService: AddressService,
-        private readonly passwordService: PasswordService
-    ) { }
+  constructor(
+    @InjectConnection() private readonly knex: Knex,
+    private readonly customerService: CustomersService,
+    private readonly userService: UserService,
+    private readonly addressService: AddressService,
+    private readonly hashingService: HashingService,
+    private readonly jwtService: JwtService,
+    private readonly tokenService: TokenService
+  ) { }
 
-    async createCustomer(customerData: CreateCustomerDto) {
+  async createCustomer(customerData: CreateCustomerDto) {
+    const userDetails = {
+      email: customerData.email,
+      first_name: customerData.first_name,
+      last_name: customerData.last_name,
+      password: await this.hashingService.hash(customerData.password),
+      phone: customerData.phone,
+      user_type: 'customer',
+    };
 
-        const userDetails = {
-            email: customerData.email,
-            first_name: customerData.first_name,
-            last_name: customerData.last_name,
-            password: await this.passwordService.hashpassword(customerData.password),
-            phone: customerData.phone,
-            user_type: "customer"
-        }
+    return this.knex.transaction(async (trx) => {
+      const user = await this.userService.create(userDetails, trx);
 
-        return this.knex.transaction(async (trx) => {
-            const user = await this.userService.create(userDetails, trx)
-            console.log(customerData, user.id);
+      const addresses = await this.addressService.create(
+        customerData.addresses,
+        user.id,
+        trx,
+      );
+      const customer = await this.customerService.create(
+        { user_id: user.id },
+        trx,
+      );
 
-            const addresses = await this.addressService.create(customerData.addresses, user.id, trx)
-            const customer = await this.customerService.create({ user_id: user.id }, trx)
+      const { password, ...userWithoutPassword } = user;
+      return {
+        user: userWithoutPassword,
+        addresses,
+        total_orders: customer.total_orders,
+        total_spent: customer.total_spent,
+      };
+    });
+  }
 
-            const { password, ...userWithoutPassword } = user
-            return {
-                user: userWithoutPassword,
-                addresses,
-                total_orders: customer.total_orders,
-                total_spent: customer.total_spent
-            }
-        })
+  async login(loginDetails: LoginRequestDTO) {
+
+    let user
+
+    try {
+
+      user = await this.userService.getUserWithEmail(loginDetails.email)
+
+    } catch (error) {
+
+      if (error instanceof UserNotFoundException) throw new UnauthorizedException("Invalid Credentials")
+
+      throw error
     }
 
-}
+    const isvalidPassword = await this.hashingService.compare(loginDetails.password, user.password)
 
+    if (!isvalidPassword) throw new UnauthorizedException("Invalid Credentials")
+
+    const payload = { userid: user.id, email: user.email, type: user.user_type }
+
+    const { refreshToken } = await this.tokenService.createRefreshToken(user.id)
+
+    const tokens = {
+      access: await this.jwtService.signAsync(payload),
+      refresh: refreshToken
+    }
+
+    return {
+      user: plainToInstance(UserResponseDto, user, { excludeExtraneousValues: true }),
+      tokens
+    }
+
+  }
+}

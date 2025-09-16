31064fceed773397214a83e29495734680b07f6a
Aliyu Efe <aliyuefe9501@gmail.com>
2025-09-16 15:47:16 +0100

    remove unused dto

--- src/modules/customers/dto/customer-response.dto.ts
+++
@@ -1,7 +0,0 @@
-import { AddressResponseDTO } from 'src/modules/addresses/dto/address-response.dto';
-import { UserResponseDto } from 'src/modules/users/dto/user-response.dto';
-
-export class CustomerResponseDto {
-  user: UserResponseDto;
-  addresses: AddressResponseDTO[];
-}

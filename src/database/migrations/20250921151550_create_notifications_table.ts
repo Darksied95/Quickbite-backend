c22704c6a4925123dcbf46902ab053eb2030b363
Aliyu Efe <aliyuefe9501@gmail.com>
2025-09-21 16:18:15 +0100

    add notification migration

---
+++ src/database/migrations/20250921151550_create_notifications_table.ts
@@ -0,0 +1,23 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable("notifications", (table) => {
+        table.uuid("id").primary().defaultTo(knex.raw("gen_random_uuid()"))
+        table.uuid("user_id").notNullable().references("id").inTable("users").onDelete('CASCADE').onUpdate('CASCADE');
+        table.enum("type", ["order", "delivery", "payment"]).notNullable();
+        table.string("title").notNullable();
+        table.text("message").notNullable();
+        table.boolean("is_read").notNullable().defaultTo(false);
+        table.timestamps(true, true);
+
+        table.index("user_id");
+        table.index("recipient_id");
+    });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable("notifications");
+}
+

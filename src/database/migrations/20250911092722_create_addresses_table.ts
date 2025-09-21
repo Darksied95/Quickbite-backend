b563f15cc33d99861deadf12f834bb6ea640d9ea
Aliyu Efe <aliyuefe9501@gmail.com>
2025-09-21 02:14:08 +0100

    remove user reference from addresses table

--- src/database/migrations/20250911092722_create_addresses_table.ts
+++ src/database/migrations/20250911092722_create_addresses_table.ts
@@ -4,13 +4,6 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema
     .createTable('addresses', function (table) {
       table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
-      table
-        .uuid('user_id')
-        .notNullable()
-        .references('id')
-        .inTable('users')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE');
       table.string('street_address').notNullable();
       table.string('apartment_unit');
       table.string('city').notNullable();

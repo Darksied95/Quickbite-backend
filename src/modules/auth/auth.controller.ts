1a53e09e88bf33cf9f581f719686fc47ef3e34ad
Aliyu Efe <aliyuefe9501@gmail.com>
2025-11-07 17:28:43 +0100

    feat: consolidate registrations to one endpoint

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,14 +1,12 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateCustomerDto } from '../customers/dto/create-customers.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { LoginRequestDTO, LoginResponseDTO } from './dto/login.dto';
-import { CreateAdminDto } from '../admin/dto/create-admin.dto';
-
-
+import { RegisterDTO } from './dto/register.dto';
 
 @ApiTags('auth')
 @Controller('auth')
+
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
@@ -18,36 +16,12 @@ export class AuthController {
     return result
   }
 
-  @Post('register/customer')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  @ApiOperation({ summary: 'Register a new customer' })
-  @ApiResponse({ status: 201, description: 'Customer successfully registered' })
-
-  async registerCustomer(@Body() req: CreateCustomerDto) {
-    return await this.authService.createCustomer(req);
+  @Post('register')
+  @ApiOperation({ summary: 'Register a new customer/driver/restaurant' })
+  @ApiResponse({ status: 201, description: 'User successfully registered' })
+  async register(@Body() req: RegisterDTO) {
+    return await this.authService.register(req)
   }
 
-  @Post("register/admin")
-  @HttpCode(HttpStatus.NO_CONTENT)
-  async createAdmin(@Body() req: CreateAdminDto) {
-    return await this.authService.createAdmin(req)
-  }
 
-  @Post('register/driver')
-  @ApiOperation({ summary: 'Register a new driver' })
-  @ApiResponse({ status: 201, description: 'Driver successfully registered' })
-  async registerDriver(@Body() req: CreateCustomerDto) {
-    return;
-  }
-
-  @Post('register/restaurant_owner')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  @ApiOperation({ summary: 'Register a new restaurant owner' })
-  @ApiResponse({
-    status: 201,
-    description: 'Restaurant successfully registered',
-  })
-  registerRestauranOwner(@Body() req: CreateCustomerDto) {
-    return this.authService.createRestaurantOwner(req)
-  }
 }

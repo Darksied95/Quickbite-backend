e25e7c7978e5b1fad5bc7cc087997ff9a8b58a92
Aliyu Efe <aliyuefe9501@gmail.com>
2025-11-10 13:16:11 +0100

    fix: remove redundant restaurant_id

--- src/database/migrations/20250921150243_create_inventories_table.ts
+++ src/database/migrations/20250921150243_create_inventories_table.ts
@@ -5,14 +5,11 @@ import { TableNames } from "../tables/table.constant";
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable(TableNames.Inventories, (table) => {
         table.uuid("id").primary().defaultTo(knex.raw("gen_random_uuid()"))
-        table.uuid("restaurant_id").notNullable().references("id").inTable(TableNames.RestaurantProfiles).onDelete('CASCADE').onUpdate('CASCADE');
         table.uuid("item_id").notNullable().references("id").inTable(TableNames.MenuItems).onDelete('CASCADE').onUpdate('CASCADE');
         table.integer("quantity").notNullable();
         table.timestamps(true, true);
 
-        table.index("restaurant_id");
         table.index("item_id");
-        table.index("quantity");
     });
 }
 
